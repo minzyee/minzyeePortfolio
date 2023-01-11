2b55d4886fbfdb38698e79ee397ad3a5732d85c7
minzyee <vvzmsqufvv@gmail.com>
2023-01-12 01:39:58 +0900

    Style: 디폴트 컬러 팔레트 셋팅

---
+++ src/commons/style/themes/default.jsx
@@ -0,0 +1,15 @@
+const themeColor = {
+	palette: {
+		white: "#FFFFFF",
+		ivory: "#FFFBF5",
+		red: "#FF625A",
+		orange: "#FBA25B",
+		yellow: "#FFD76D",
+		green: "#69B060",
+		blue: "#8BAAF8",
+		pink: "#F8D5FA",
+		black: "#000000",
+	},
+};
+
+export default themeColor;
